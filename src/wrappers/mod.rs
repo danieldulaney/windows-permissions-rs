06ebecbbad61641b56ce901b20abea660bdd6c82
Daniel <dulaney.daniel@gmail.com>
2019-01-29 01:34:41 -0500

    Added some integration testing for WinAPI wrappers

--- src/wrappers/mod.rs
+++ src/wrappers/mod.rs
@@ -14,3 +14,40 @@ pub use create_well_known_sid::CreateWellKnownSid;
 pub use equal_sid::EqualSid;
 pub use get_named_security_info::GetNamedSecurityInfo;
 pub use lookup_account_sid::LookupAccountSid;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use std::path::Path;
+    use winapi::um::winnt::{self, WinLocalSid, WinWorldSid};
+    use winapi::um::accctrl::SE_FILE_OBJECT;
+
+    const SEC_INFO: u32 = winnt::OWNER_SECURITY_INFORMATION
+        | winnt::GROUP_SECURITY_INFORMATION
+        | winnt::DACL_SECURITY_INFORMATION;
+
+    #[test]
+    fn well_known_sids_are_equal() {
+        let world_sid_1 = CreateWellKnownSid(WinWorldSid).unwrap();
+        let world_sid_2 = CreateWellKnownSid(WinWorldSid).unwrap();
+        let local_sid_1 = CreateWellKnownSid(WinLocalSid).unwrap();
+        let local_sid_2 = CreateWellKnownSid(WinLocalSid).unwrap();
+
+        assert!(EqualSid(&world_sid_1, &world_sid_2));
+        assert!(EqualSid(&local_sid_1, &local_sid_2));
+        assert!(!EqualSid(&world_sid_1, &local_sid_2));
+        assert!(!EqualSid(&local_sid_1, &world_sid_2));
+    }
+
+    #[test]
+    fn cargo_toml_is_owned() {
+        let cargo_toml_path =
+            Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("Cargo.toml");
+
+        let sd = GetNamedSecurityInfo(cargo_toml_path.as_os_str(), SE_FILE_OBJECT, SEC_INFO).unwrap();
+
+        assert!(sd.owner().is_some());
+        assert!(sd.group().is_some());
+    }
+}
