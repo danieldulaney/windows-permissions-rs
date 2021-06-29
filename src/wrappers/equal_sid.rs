19e332b726b94ddd5a0c7544ad19e57436f671dc
Daniel Dulaney <dulaney.daniel@gmail.com>
2021-06-29 16:17:55 -0400

    Add doctest for EqualSid

--- src/wrappers/equal_sid.rs
+++ src/wrappers/equal_sid.rs
@@ -1,9 +1,22 @@
 use crate::Sid;
 
-/// Wraps [`EqualSid`](https://docs.microsoft.com/en-us/windows/win32/api/securitybaseapi/nf-securitybaseapi-equalsid)
+/// Wraps [`EqualSid`](https://docs.microsoft.com/en-us/windows/win32/api/securitybaseapi/nf-securitybaseapi-equalsid).
 ///
 /// Because `&Sid` is a safe Rust construct, it must be valid. Therefore, it is
 /// impossible for this function to error.
+///
+/// ```
+/// use windows_permissions::{Sid, LocalBox, wrappers::EqualSid};
+/// use winapi::um::winnt::WinCreatorGroupSid;
+///
+/// let sid1: LocalBox<Sid> = "S-1-3-1".parse().unwrap();
+/// let sid2 = Sid::well_known_sid(WinCreatorGroupSid).unwrap();
+///
+/// let sid3 = Sid::new([0, 0, 0, 0, 0, 3], &[2]).unwrap();
+///
+/// assert!(EqualSid(&sid1, &sid2));
+/// assert!(!EqualSid(&sid1, &sid3));
+/// ```
 #[allow(non_snake_case)]
 pub fn EqualSid(sid1: &Sid, sid2: &Sid) -> bool {
     (unsafe {
