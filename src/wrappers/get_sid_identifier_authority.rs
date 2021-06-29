30d06bd790c7c8bf73207481d986d03993c427f9
Daniel Dulaney <dulaney.daniel@gmail.com>
2021-06-29 16:28:53 -0400

    Add doctest for GetSidIdentifierAuthority

--- src/wrappers/get_sid_identifier_authority.rs
+++ src/wrappers/get_sid_identifier_authority.rs
@@ -1,6 +1,15 @@
 use crate::Sid;
 
-/// Wraps [`GetSidIdentifierAuthority`](https://docs.microsoft.com/en-us/windows/win32/api/securitybaseapi/nf-securitybaseapi-getsididentifierauthority)
+/// Wraps [`GetSidIdentifierAuthority`](https://docs.microsoft.com/en-us/windows/win32/api/securitybaseapi/nf-securitybaseapi-getsididentifierauthority).
+///
+/// ```
+/// use windows_permissions::{Sid, wrappers::GetSidIdentifierAuthority};
+///
+/// let id_auth: [u8; 6] = *b"Hello!";
+/// let sid = Sid::new(id_auth, &[1, 2, 3]).unwrap();
+///
+/// assert_eq!(&id_auth, GetSidIdentifierAuthority(&sid));
+/// ```
 #[allow(non_snake_case)]
 pub fn GetSidIdentifierAuthority(sid: &Sid) -> &[u8; 6] {
     let ptr = unsafe {
